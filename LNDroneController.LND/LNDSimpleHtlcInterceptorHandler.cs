d7f487954a36ed1439cb4c9d98930a86312ca952
Richard Safier <richardj@safier.com>
2022-03-18 20:07:07 -0400

    add simple interceptor logic

---
+++ LNDroneController.LND/LNDSimpleHtlcInterceptorHandler.cs
@@ -0,0 +1,55 @@
+using Grpc.Core;
+using Lnrpc;
+using System;
+using System.Threading.Tasks;
+using Routerrpc;
+using System.Diagnostics;
+using ServiceStack.Text;
+
+namespace LNDroneController.LND
+{
+    /// <summary>
+    /// This is a very simple interceptor loop, it will block and requires response before moving to next HTLC event.
+    /// Not for production use, but good for testing
+    /// </summary>
+    public class LNDSimpleHtlcInterceptorHandler
+    {
+        public LNDNodeConnection Node { get; }
+        public event Func<ForwardHtlcInterceptRequest,ForwardHtlcInterceptResponse> OnIntercept;
+
+        public LNDSimpleHtlcInterceptorHandler(LNDNodeConnection connection, Func<ForwardHtlcInterceptRequest, ForwardHtlcInterceptResponse> interceptLogic = null)
+        {
+            Node = connection;
+            Task.Factory.StartNew(AttachInterceptor);
+            if (interceptLogic == null)
+            {
+                OnIntercept = (data) =>
+                {
+                    Debug.Print(data.Dump());
+                    return new ForwardHtlcInterceptResponse
+                    {
+                        Action = ResolveHoldForwardAction.Resume,
+                        IncomingCircuitKey = data.IncomingCircuitKey,
+                    };
+                };
+            }
+            else
+            {
+                OnIntercept = interceptLogic;
+            }
+        }
+
+        private async Task AttachInterceptor()
+        {
+            using (var streamingEvents = Node.RouterClient.HtlcInterceptor())
+            {
+                while (await streamingEvents.ResponseStream.MoveNext())
+                {
+                    var message = streamingEvents.ResponseStream.Current;
+                    var result = OnIntercept(message);
+                    await streamingEvents.RequestStream.WriteAsync(result);
+                }
+            }
+        }
+    }
+}
