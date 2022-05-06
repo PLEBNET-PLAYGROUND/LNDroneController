ffb87c2461657e09ef8aaf6e5950fdf05ff0f05f
Richard Safier <richardj@safier.com>
2022-05-06 13:48:03 -0400

    CLN protobuff

---
+++ LNDroneController.CLN/CLNSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace LNDroneController.CLN
+{
+    public class CLNSettings
+    {
+        /// <summary>
+        /// Grpc endpoint
+        /// </summary>
+        public string GrpcEndpoint { get; set; }
+        /// <summary>
+        /// TLS Cert as Base64 string, if provided will be perfered source
+        /// </summary>
+        public string TLSCertBase64 { get; set; }
+        /// <summary>
+        /// Macaroon Path
+        /// </summary>
+        public string MacaroonBase64 { get; set; }
+        /// <summary>
+        /// Default Fee Maximum as a percentage of total amount of invoice
+        /// </summary>
+    }
+}
---
+++ LNDroneController.CLN/update-protofiles.sh
@@ -0,0 +1,3 @@
+ #! /bin/bash
+wget -O ./Grpc/node.proto https://raw.githubusercontent.com/ElementsProject/lightning/master/cln-grpc/proto/node.proto
+wget -O ./Grpc/primitives.proto https://raw.githubusercontent.com/ElementsProject/lightning/master/cln-grpc/proto/primitives.proto
