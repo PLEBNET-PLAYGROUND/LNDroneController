83021550f2888d03ccb26a07409bd86805833a3f
Richard Safier <richardj@safier.com>
2022-08-23 11:59:28 -0400

    cleanup

--- LNDroneController.LND/LNDChannelInterceptorHandler.cs
+++ LNDroneController.LND/LNDChannelInterceptorHandler.cs
@@ -19,16 +19,6 @@ namespace LNDroneController.LND
             Task.Factory.StartNew(ListenForCustomMessages);
         }
 
-        public async Task<SendCustomMessageResponse> SendCustomMessageRequest(CustomMessage m)
-        {
-            return await Node.LightningClient.SendCustomMessageAsync(new SendCustomMessageRequest
-            {
-                Data = m.Data,
-                Peer = m.Peer,
-                Type = m.Type,
-            });
-        }
-
         private async Task ListenForCustomMessages()
         {
             using (var streamingEvents = Node.LightningClient.ChannelAcceptor())
